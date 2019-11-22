699d1f2e60966e4eacf51fda255bab109b5ed818
Floris <Flomo123456789@gmail.com>
2019-11-22 13:31:37 +0100

    Updated README and changed project

--- WeightedListPicker/WeightedListPicker.py
+++ WeightedListPicker/WeightedListPicker.py
@@ -1,25 +1,24 @@
 from random import randint
 
-class WeightedListPicker:
-    
-    def RandomList(aList, chances):
-        package = aList
-        result = 0
-        maxScore = 0
-        catagory = 0
+ 
+def RandomList(aList, chances):
+    package = aList
+    result = 0
+    maxScore = 0
+    catagory = 0
         
-        # add up all the score from chances
-        for i in chances:
-            maxScore += i
+    # add up all the score from chances
+    for i in chances:
+        maxScore += i
 
-        # make random int to decide what the score is
-        score = randint(0, maxScore)
+    # make random int to decide what the score is
+    score = randint(0, maxScore)
         
-        # check in what catagory score lands
-        for i in chances:
-            catagory += i
-            if catagory >= score:
-                # decide what list item to pick
-                package = aList[result]
-                return package
-            result += 1
+    # check in what catagory score lands
+    for i in chances:
+        catagory += i
+        if catagory >= score:
+            # decide what list item to pick
+            package = aList[result]
+            return package
+        result += 1
